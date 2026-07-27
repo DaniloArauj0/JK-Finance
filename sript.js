fe72cc3ef54cc8dfbdb080224772b31247b860cc
DaniloArauj0 <danilodev07@bol.com.br>
2026-07-27 12:48:09 -0300

    teste da biblioteca
    teste biblioteca

---
+++ sript.js
@@ -0,0 +1,10 @@
+const typed = new Typed (".teste",{
+    strings: ["JK FINANCE", "INVESTINDO NOS SEUS SONHOS"],
+    typeSpeed: 80,
+    backSpeed: 60,
+    backDelay: 1200,      // pausa antes de apagar
+    loop: true,
+    showCursor: false  // Oculta o cursor de digitação
+} );
+
+
